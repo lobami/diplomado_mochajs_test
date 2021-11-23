54362da7549d029fc6df6189767993588f101130
Loth Mejia Martinez <42503437+lobami@users.noreply.github.com>
2021-11-22 19:41:16 -0600

    feature(test): FIX sort test

--- test/sort_array.test.js
+++ test/sort_array.test.js
@@ -7,10 +7,10 @@ const work = require('../src/work')
 describe('ordenar array', () => {
     describe('ordenar array', () => {
         it('ordenar array', () => {
-            expect(work.sort_number([4, 3])).to.equal([ 3, 4 ]);
-            expect(work.sort_number([5, 3])).to.equal([3, 5]);
-            expect(work.sort_number([1, 3])).to.equal([1, 3]);
-            expect(work.sort_number([8, 7])).to.equal([7, 8]);
+            expect(work.sort_number([4, 3])).to.deep.equal([3, 4]);
+            expect(work.sort_number([5, 3])).to.deep.equal([3, 5]);
+            expect(work.sort_number([1, 3])).to.deep.equal([1, 3]);
+            expect(work.sort_number([8, 7])).to.deep.equal([7, 8]);
         });
     });
 
